ef5e06aff0ccccb99bcc3e93548212ee3836c4c3
mbaj <119751248+m-baj@users.noreply.github.com>
2024-06-12 14:40:33 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,19 +1,69 @@
 import socket
+import math
+from matplotlib import pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
-# Adres i port serwera UDP
-server_address = ("127.0.0.1", 1234)
+HOST = "10.42.0.200"
+PORT = 8080
 
-# Tworzenie gniazda UDP
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+MAX_DEG = 180
+MAX_DISTANCE = 350
 
-try:
-    # Wysłanie wiadomości do serwera
-    message = "Hello from client"
-    client_socket.sendto(message.encode(), server_address)
 
-    # Oczekiwanie na odpowiedź z serwera
-    response, server_address = client_socket.recvfrom(1024)
-    print("Received from server:", response.decode())
-finally:
-    # Zamknięcie gniazda
-    client_socket.close()
+def get_plot_coordinates(degree, distance):
+    x = distance * math.sin(math.radians(degree))
+    y = distance * math.cos(math.radians(degree))
+    return x, y
+
+
+def prepare_plot():
+    plt.ion()
+    fig, ax = plt.subplots(figsize=(8, 8))
+    plt.title("lidar-servo")
+    plt.xlabel("distance [mm]")
+    plt.ylabel("distance [mm]")
+    plt.gca().set_aspect('equal')
+
+    ax.set_xlim(-10, MAX_DISTANCE)
+    ax.set_ylim(-MAX_DISTANCE, MAX_DISTANCE)
+    plt.show()
+
+    return fig, ax
+
+
+def get_line_color(distance):
+    if distance < 50:
+        return 'red'
+    elif distance < 100:
+        return 'yellow'
+    else:
+        return 'green'
+
+
+def update_plot(fig, lines, degree, distance):
+    x, y = get_plot_coordinates(degree, distance)
+    lines[degree].set_data([0, x], [0, y])
+    lines[degree].set_color(get_line_color(distance))
+
+    fig.canvas.draw_idle()
+    fig.canvas.flush_events()
+
+
+def main():
+    fig, ax = prepare_plot()
+    lines = [ax.plot([0, 0], [0, 0], '-o')[0] for _ in range(MAX_DEG + 1)]
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b"Hello, world")
+
+        while True:
+            measurement_data = s.recv(256).decode("utf-8").split(",")
+            degree = int(measurement_data[0])
+            distance = int(measurement_data[1])
+
+            update_plot(fig, lines, degree, distance)
+
+
+if __name__ == "__main__":
+    main()
